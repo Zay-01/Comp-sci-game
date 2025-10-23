9f5b75b73981bf4c52023bb048f12a6c1bffdde5
Zay <mobmine7@gmail.com>
2025-10-23 01:25:02 -0400

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -4,22 +4,19 @@ public class Enemy
     private int attack;
     private double defense;
     private int health;
-    private int shield;
     private String name;
 
     /**
      * @param Attack
      * @param Defense
      * @param Health
-     * @param Shield
      * @param Name
      */
-    public Enemy(int Attack, Double Defense, int Health, int Shield, String Name)
+    public Enemy(int Attack, Double Defense, int Health, String Name)
     {
         attack = Attack;
         defense = Defense;
         health = Health;
-        shield = Shield;
         name = Name;
     }
 
@@ -28,20 +25,14 @@ public class Enemy
         return name;
     }
 
-    public boolean killedCheck()
+    public boolean isAlive()
     {
-        return health <= 0;
+        return health > 0;
     }
 
-    /**
-     * Shield reduces it by a certain % due to armor reducing how much the enemy swings or attacks you
-     * the weight of armor is heavy after all cant have much heavy swings 
-     * @return attack amnt
-     */
-    public void Attack()
+    public int Attack()
     {
-        health -= attack - (attack*shield);
+        return attack;
     }
-
 
 }
