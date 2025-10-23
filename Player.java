1bbc7d504f0bc05d29d5142068275ba147eab07a
Zay <mobmine7@gmail.com>
2025-10-23 15:49:47 -0400

    e

--- Player.java
+++ Player.java
@@ -1,46 +1,15 @@
 public class Player
 {
-    private int attack,health,mana;
-    private double defense;
-    private final int maxHealth, maxMana;
+    int[] attack = {10,20,30,40,50};
+    int[] health = {100,200,300,400,500};
+    int[] mana = {100,120,140,160,180};
+    double[] defense = {.1,.2,.3,.4,.5};
 
-    /**
-     * @param Attack
-     * @param Defense
-     * @param Health
-     * @param Mana
-     */
-    public Player(int Attack,double Defense,int Health,int Mana)
-    {
-        attack = Attack;
-        defense = Defense;
-        health = Health;
-        mana = Mana;
-        maxHealth = Health;
-        maxMana = Mana;
-    }
-    
-    /**
-     * @param actionIndex Takes you current action options
-     * @return Which action you want to do and a integer of what there currently is
-     */
-    public String actions(int actionIndex)
-    {
+    private int currentLevel = 0;
+    private int currentXp = 0;
+    private final int[] maxHealth = {100,200,300,400,500};
+    private final int[] maxMana = {100,120,140,160,180};
 
-            switch(actionIndex)
-            {
-            case 1:
-            return "Attack " + attack;
-            case 2:
-            return "Defend " + defense;
-            case 3:
-            return "Mana " + mana;
-            case 4: 
-            return "Heal " + 30;
-            }
-        
-        return "[ERROR Did you figure this out?? - Player -> Actions]";
-    }
 
     /**
      * @param damage How much to remove from players health
@@ -48,38 +17,62 @@ public class Player
      */
     public void damageRecieved(int damage)
     {
-        health -= damage-((int)(damage*defense));
+        health[currentLevel] -= damage-((int)(damage*defense[currentLevel]));
     }
 
+    /**
+     * @param xp XP gained from monster
+     * level up if required xp is reached and currentLevel is not the limit
+     * currentLevel - 1 should be used instead of the number
+     */
+    public void gainedXp(int xp)
+    {
+        currentXp += xp;
+        if(currentLevel == 1000 && currentLevel != 4)
+        {
+            currentXp -= 1000;
+            currentLevel += 1;
+        }
+    }
 
     /**
-     * @param heal 
-     * How much player healed up
+     * @param heal - How much player healed up
      */
-    public void amountHealed(int heal)
+    public String amountHealed(int heal)
     {
-        health += heal;
-        if(health > maxHealth)
+        health[currentLevel] += heal;
+
+        if(health[currentLevel] > maxHealth[currentLevel])
         {
             health = maxHealth;
         }
+        return "Healed: " + heal + " Current HP: " + health[currentLevel];
     }
 
-    public void manaUsed(int used)
+    public String manaUsed(int Mana)
     {
-        mana -= used;
-        if(mana <= 0)
+        mana[currentLevel] -= Mana;
+
+        if(mana[currentLevel] <= 0)
         {
-            health -= (int)(used*defense);
+            health[currentLevel] -= (int)(Mana*defense[currentLevel]);
+
+            return " [] " + Mana + " [MANA] " + mana[currentLevel] + " [HP]";
+
         }
+        
+        return "Mana Restored: " + Mana + " Current Mana: " + mana[currentLevel];
+
+
     }
+
     public boolean isAlive()
     {
-        return health > 0;
+        return health[currentLevel] > 0;
     }
 
     public String toString()
     {
-        return "\n[HEALTH]: " + health + "\n[DEFENSE]: " + defense + "\n[ATTACK]: " + attack + "\n[MANA]: " + mana + "\n[MAX-HEALTH]: " + maxHealth + "\n[MAX-MANA]: " + maxMana + "\n[ALIVE]: " + isAlive();
+        return "\n[HEALTH]: " + health[currentLevel] + "\n[DEFENSE]: " + defense[currentLevel] + "\n[ATTACK]: " + attack[currentLevel] + "\n[MANA]: " + mana[currentLevel] + "\n\n[MAX-HEALTH]: " + maxHealth[currentLevel] + "\n[MAX-MANA]: " + maxMana[currentLevel] + "\n\n[ALIVE]: " + isAlive() + "\n[LEVEL]: " + currentLevel + "\n[XP]: " + currentXp;
     }
 }
---
+++ TestDriver.java
@@ -0,0 +1,12 @@
+public class TestDriver
+{
+
+    public static void main(String[] args) 
+    {
+
+        Player test = new Player();
+        test.damageRecieved(90);
+        System.out.println(test);
+        System.out.println(test.);
+    }
+}
